fff9b6f0cf14d55b5cab1454a5c913c3518e78f1
Phil Austin <paustin@eos.ubc.ca>
2019-02-16 14:10:38 -0800

    initial

---
+++ talk_dir/python/mcgill_talk.py
@@ -0,0 +1,87 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     notebook_metadata_filter: all
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.0.0-rc5
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown] {"toc": true, "slideshow": {"slide_type": "slide"}}
+# <h1>Table of Contents<span class="tocSkip"></span></h1>
+# <div class="toc"><ul class="toc-item"><li><span><a href="#Part-1----Infrastructure" data-toc-modified-id="Part-1----Infrastructure-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Part 1 -- Infrastructure</a></span></li><li><span><a href="#Part-2----Examples" data-toc-modified-id="Part-2----Examples-2"><span class="toc-item-num">2&nbsp;&nbsp;</span>Part 2 -- Examples</a></span></li><li><span><a href="#Some-history" data-toc-modified-id="Some-history-3"><span class="toc-item-num">3&nbsp;&nbsp;</span>Some history</a></span></li><li><span><a href="#Infrastructure" data-toc-modified-id="Infrastructure-4"><span class="toc-item-num">4&nbsp;&nbsp;</span>Infrastructure</a></span><ul class="toc-item"><li><span><a href="#Linux-kernel" data-toc-modified-id="Linux-kernel-4.1"><span class="toc-item-num">4.1&nbsp;&nbsp;</span>Linux kernel</a></span></li><li><span><a href="#Git-and-Github" data-toc-modified-id="Git-and-Github-4.2"><span class="toc-item-num">4.2&nbsp;&nbsp;</span>Git and Github</a></span></li></ul></li><li><span><a href="#Conda-forge" data-toc-modified-id="Conda-forge-5"><span class="toc-item-num">5&nbsp;&nbsp;</span>Conda-forge</a></span></li></ul></div>
+
+# %% {"slideshow": {"slide_type": "skip"}}
+from IPython.display import Image
+
+# %% [markdown] {"slideshow": {"slide_type": "slide"}}
+# # Computational instruction with Jupyter notebooks
+#
+# Philip Austin, Department of Earth, Ocean and Atmospheric Sciences, UBC
+#
+# Slides: 
+
+# %% [markdown] {"slideshow": {"slide_type": "subslide"}}
+# ## Part 1 -- Infrastructure
+#
+# * Linux/git
+# * Conda-forge
+# * Jupyter
+# * Jupytext
+# * Cocalc
+# * pangeo
+
+# %% [markdown] {"slideshow": {"slide_type": "subslide"}}
+# ## Part 2 -- Examples
+#
+# * 2nd year course, 48 students
+#   - Jupyter/chrome/cocalc
+# * 3rd/4th year course, 26 students 
+#   - Jupyter/laptops/conda-forge
+# * Grad/postdoc workshop, 20 students, 
+#   - pangeo with dask/xarray,zarr on an HPC cluster
+#
+
+# %% [markdown] {"slideshow": {"slide_type": "slide"}}
+# ## Some history
+#
+# **July, 1997: BAMS article: John Stockie, Susan Allen, Phil Austin**
+#
+# *An Interactive Course in Numerical Methods for the Earth Sciences*
+
+# %% {"slideshow": {"slide_type": "subslide"}}
+Image('figures/bams_ref.png')
+
+# %% [markdown] {"trusted": false, "slideshow": {"slide_type": "slide"}}
+# ## Infrastructure
+#
+# ### Linux kernel 
+#
+# * developed by 19,000 unique committers
+#
+# * 65,000 files
+#
+# * 25 million lines of code
+
+# %% [markdown] {"trusted": false, "slideshow": {"slide_type": "subslide"}}
+# ### Git and Github
+#
+# * 2005: [Linus Torvalds](https://en.wikipedia.org/wiki/Linus_Torvalds) begins [Git development](https://en.wikipedia.org/wiki/Git) 
+#
+# * 2008: [Github founded](https://en.wikipedia.org/wiki/GitHub), in 2018 it hosted 28 million users and 57 million repositories
+# * Distributed version control makes possible the:
+#     - [pull request workflow](https://yangsu.github.io/pull-request-tutorial/)
+
+# %% [markdown] {"slideshow": {"slide_type": "subslide"}}
+# ## Conda-forge
+#
+# # 
+
+# %%
